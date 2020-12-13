a952565e7053d90f6231ed2d49a963179dab1eef
Colin Gillespie <ratchetandcolin@gmail.com>
2020-12-13 16:26:01 -0500

    Add function for sorting store array

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,3 +4,16 @@ export const swap = (array, index1, index2) => {
     [newArray[index1], newArray[index2]] = [newArray[index2], newArray[index1]];
     return newArray;
 };
+
+export const sortBy = (array, direction) => {
+    if (!direction) {
+        return array;
+    }
+    if (direction === 'asc') {
+        return array.sort((a, b) => a.createdAt - b.createdAt);
+    } else if (direction === 'desc') {
+        return array.sort((a, b) => b.createdAt - a.createdAt);
+    } else {
+        return array;
+    }
+};
