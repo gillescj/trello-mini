9d9b453826ccd572dbca29fbb628f59304dbc55b
Colin Gillespie <ratchetandcolin@gmail.com>
2020-12-30 17:16:11 -0500

    Add id to card objects, add card functions

--- src/store/stores.js
+++ src/store/stores.js
@@ -8,8 +8,8 @@ const sampleData = [
         name: 'Current Tasks',
         sortOrder: 'asc',
         cards: [
-            { title: 'Redesign Project A3', createdAt: 1 },
-            { title: 'Research for Potential Lead', createdAt: 2 },
+            { title: 'Redesign Project A3', createdAt: 1, id: uniqueId('card-') },
+            { title: 'Research for Potential Lead', createdAt: 2, id: uniqueId('card-') },
         ],
     },
     {
@@ -17,9 +17,17 @@ const sampleData = [
         name: 'Bugs',
         sortOrder: 'asc',
         cards: [
-            { title: 'Something wrong with x', createdAt: 1 },
-            { title: 'Header scroll glitch on mobile', createdAt: 2 },
-            { title: 'Onclick not working in chrome', createdAt: 3 },
+            { title: 'Something wrong with x', createdAt: 3, id: uniqueId('card-') },
+            {
+                title: 'Header scroll glitch on mobile',
+                createdAt: 4,
+                id: uniqueId('card-'),
+            },
+            {
+                title: 'Onclick not working in chrome',
+                createdAt: 5,
+                id: uniqueId('card-'),
+            },
         ],
     },
     {
@@ -27,8 +35,8 @@ const sampleData = [
         name: 'Finished Tasks',
         sortOrder: 'asc',
         cards: [
-            { title: 'Project A2', createdAt: 1 },
-            { title: 'Winter Campaign', createdAt: 2 },
+            { title: 'Project A2', createdAt: 6, id: uniqueId('card-') },
+            { title: 'Winter Campaign', createdAt: 7, id: uniqueId('card-') },
         ],
     },
 ];
@@ -96,6 +104,33 @@ function createBoardStore(key, startValue) {
             update((store) => {
                 const index = store.findIndex((list) => list.id === id);
                 store[index].sortOrder = order;
+                return store;
+            });
+        },
+        removeCard: (id, createdAt) => {
+            update((store) => {
+                const index = store.findIndex((list) => list.id === id);
+                store[index].cards = store[index].cards.filter(
+                    (card) => card.createdAt !== createdAt
+                );
+                return store;
+            });
+        },
+        moveCard: (listID, cardID, where) => {
+            update((store) => {
+                const targetIndex = store.findIndex((list) => list.id === listID);
+                const newIndex = where === 'left' ? targetIndex - 1 : targetIndex + 1;
+                if (newIndex < 0 || newIndex > store.length - 1) {
+                    return store;
+                }
+                const card = store[targetIndex].cards.filter(
+                    (card) => card.id === cardID
+                )[0];
+                const previousListCards = store[targetIndex].cards.filter(
+                    (card) => card.id !== cardID
+                );
+                store[targetIndex].cards = previousListCards;
+                store[newIndex].cards = [...store[newIndex].cards, card];
                 return store;
             });
         },
